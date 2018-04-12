d15553696e50c71cf6feeb29ff3742e436a67c69
Yahya Hussein <yahyahussein91@gmail.com>
2018-04-12 14:57:05 -0700

    added automatic imports

--- ButtonClickApp/app/src/main/java/com/example/yhussein/buttonclickapp/MainActivity.java
+++ ButtonClickApp/app/src/main/java/com/example/yhussein/buttonclickapp/MainActivity.java
@@ -2,9 +2,12 @@ package com.example.yhussein.buttonclickapp;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.EditText;
 
 //Class that allows backwards compatibility from import statement
 public class MainActivity extends AppCompatActivity {
+    private EditText editText;
+    private
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
