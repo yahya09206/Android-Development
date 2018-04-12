fba4e2d011c00cdd344ed4caebb90f500539b52f
Yahya Hussein <yahyahussein91@gmail.com>
2018-04-12 15:05:43 -0700

    added more comments

--- ButtonClickApp/app/src/main/java/com/example/yhussein/buttonclickapp/MainActivity.java
+++ ButtonClickApp/app/src/main/java/com/example/yhussein/buttonclickapp/MainActivity.java
@@ -2,17 +2,26 @@ package com.example.yhussein.buttonclickapp;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
 
 //Class that allows backwards compatibility from import statement
 public class MainActivity extends AppCompatActivity {
-    private EditText editText;
-    private
+    //type of objects to be dealing with
+    private EditText userInput;
+    private Button button;
+    private TextView textView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         //sets which layout to use
         setContentView(R.layout.activity_main);
+
+        //associate fields with the layout. findviewbyid returns the view object based on which id provided
+        userInput = (EditText) findViewById(R.id.editText);
+        button = (Button) findViewById(R.id.button);
+        textView = (TextView) findViewById((R.id.textView));
     }
 }
