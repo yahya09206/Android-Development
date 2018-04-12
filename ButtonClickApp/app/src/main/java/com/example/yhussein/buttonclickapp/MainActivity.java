f9008df5aa9a268a5564c48be65d0629f086b891
Yahya Hussein <yahyahussein91@gmail.com>
2018-04-12 14:44:06 -0700

    added comments

--- ButtonClickApp/app/src/main/java/com/example/yhussein/buttonclickapp/MainActivity.java
+++ ButtonClickApp/app/src/main/java/com/example/yhussein/buttonclickapp/MainActivity.java
@@ -3,11 +3,13 @@ package com.example.yhussein.buttonclickapp;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+//Class that allows backwards compatibility from import statement
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        //sets which layout to use
         setContentView(R.layout.activity_main);
     }
 }
