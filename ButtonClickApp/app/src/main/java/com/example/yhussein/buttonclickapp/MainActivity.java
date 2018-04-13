fa7e40fd962a7889366ef43de35b94fceb63f4db
Yahya Hussein <yahyahussein91@gmail.com>
2018-04-13 10:40:16 -0700

    fixed new line error

--- ButtonClickApp/app/src/main/java/com/example/yhussein/buttonclickapp/MainActivity.java
+++ ButtonClickApp/app/src/main/java/com/example/yhussein/buttonclickapp/MainActivity.java
@@ -32,7 +32,7 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View view) {
                 String result = userInput.getText().toString();
-                result += "/n";
+                result += "\n";
                 textView.append(result);
             }
         };
