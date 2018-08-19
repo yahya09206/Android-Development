9bba3546947473481a8e155ccb7d77c0b9cb38ec
Yahya Hussein <yahyahussein91@gmail.com>
2018-08-18 21:21:58 -0700

    temp textview fix

--- HelloWorld/app/src/main/java/com/example/yhussein/helloworld/MainActivity.java
+++ HelloWorld/app/src/main/java/com/example/yhussein/helloworld/MainActivity.java
@@ -5,12 +5,12 @@ import android.os.Bundle;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
-    private TextView textView;
+//    private TextView textView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        textView = findViewById((R.id.textView));
+//        textView = findViewById((R.id.textView));
     }
 }
