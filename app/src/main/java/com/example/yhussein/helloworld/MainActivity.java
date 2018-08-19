d113b4c47e52a44069efe9416878e37d1fe8628e
Yahya Hussein <yahyahussein91@gmail.com>
2018-08-19 15:14:36 -0700

    refresher

--- app/src/main/java/com/example/yhussein/helloworld/MainActivity.java
+++ app/src/main/java/com/example/yhussein/helloworld/MainActivity.java
@@ -2,15 +2,12 @@ package com.example.yhussein.helloworld;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
-//    private TextView textView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-//        textView = findViewById((R.id.textView));
     }
 }
