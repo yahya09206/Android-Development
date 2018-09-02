09f7c90815753410b428346d3166846b34dbacac
Yahya Hussein <yahyahussein91@gmail.com>
2018-09-02 13:04:00 -0700

    declaring fields

--- FunFacts/app/src/main/java/com/example/yhussein/funfacts/MainActivity.java
+++ FunFacts/app/src/main/java/com/example/yhussein/funfacts/MainActivity.java
@@ -2,10 +2,15 @@ package com.example.yhussein.funfacts;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.TextView;
 
 import com.example.yhussein.funfacts.R;
 
 public class MainActivity extends AppCompatActivity {
+    // Declare our View variables
+    private TextView factTextView;
+    private Button button;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -13,3 +18,7 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
     }
 }
+
+
+// An activity is a screen that users can interact with
+// A field is inside the class but outside of other methods
