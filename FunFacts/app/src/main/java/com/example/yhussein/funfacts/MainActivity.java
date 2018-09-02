203f1c63419fa923abd877eeca0736e360f2e768
Yahya Hussein <yahyahussein91@gmail.com>
2018-09-02 13:16:43 -0700

    assigning views

--- FunFacts/app/src/main/java/com/example/yhussein/funfacts/MainActivity.java
+++ FunFacts/app/src/main/java/com/example/yhussein/funfacts/MainActivity.java
@@ -10,15 +10,21 @@ import com.example.yhussein.funfacts.R;
 public class MainActivity extends AppCompatActivity {
     // Declare our View variables
     private TextView factTextView;
-    private Button button;
+    private Button showFactButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // Assign the Views from the layout file to the corresponding variables
+        factTextView = findViewById(R.id.factTextView);
+        showFactButton = findViewById(R.id.showFactButton);
     }
 }
 
 
 // An activity is a screen that users can interact with
 // A field is inside the class but outside of other methods
+// Assign views after setcontentview
+// R stands for resources
