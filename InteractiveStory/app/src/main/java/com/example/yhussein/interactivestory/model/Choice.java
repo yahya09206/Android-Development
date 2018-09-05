a3ff2028da0a44de715936d43902302b2869ddd9
Yahya Hussein <yahyahussein91@gmail.com>
2018-09-05 10:55:55 -0700

    created gets and sets for choices class

--- InteractiveStory/app/src/main/java/com/example/yhussein/interactivestory/model/Choice.java
+++ InteractiveStory/app/src/main/java/com/example/yhussein/interactivestory/model/Choice.java
@@ -3,4 +3,20 @@ package com.example.yhussein.interactivestory.model;
 public class Choice {
     private int textId;
     private int nextPage;
+
+    public int getTextId() {
+        return textId;
+    }
+
+    public void setTextId(int textId) {
+        this.textId = textId;
+    }
+
+    public int getNextPage() {
+        return nextPage;
+    }
+
+    public void setNextPage(int nextPage) {
+        this.nextPage = nextPage;
+    }
 }
