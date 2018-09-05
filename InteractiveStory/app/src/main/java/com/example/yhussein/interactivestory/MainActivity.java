0fd28fe77edfd4676baf26238a2d04a0161c7f91
Yahya Hussein <yahyahussein91@gmail.com>
2018-09-04 23:09:44 -0700

    creating edit text code

--- InteractiveStory/app/src/main/java/com/example/yhussein/interactivestory/MainActivity.java
+++ InteractiveStory/app/src/main/java/com/example/yhussein/interactivestory/MainActivity.java
@@ -2,11 +2,14 @@ package com.example.yhussein.interactivestory;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        // Name field
+        private EditText nameField;
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
