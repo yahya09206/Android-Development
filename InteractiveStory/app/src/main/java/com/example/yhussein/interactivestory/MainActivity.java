b93998cfbcf7944cd3ef0d40bc18ec0e6a56491f
Yahya Hussein <yahyahussein91@gmail.com>
2018-09-04 23:14:04 -0700

    fixed field error

--- InteractiveStory/app/src/main/java/com/example/yhussein/interactivestory/MainActivity.java
+++ InteractiveStory/app/src/main/java/com/example/yhussein/interactivestory/MainActivity.java
@@ -2,15 +2,31 @@ package com.example.yhussein.interactivestory;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
+    private EditText nameField;
+    private Button startButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         // Name field
-        private EditText nameField;
+
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        nameField = findViewById(R.id.nameEditText);
+        startButton = findViewById(R.id.startButton);
+
+        //On click listener for button
+        startButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String name = nameField.getText();
+            }
+        });
     }
 }
