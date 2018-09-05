98e090d7f1fe3d58598de2dc53dbe29117bd012b
Yahya Hussein <yahyahussein91@gmail.com>
2018-09-05 10:55:03 -0700

    created getters and setters

---
+++ InteractiveStory/app/src/main/java/com/example/yhussein/interactivestory/model/Choice.java
@@ -0,0 +1,6 @@
+package com.example.yhussein.interactivestory.model;
+
+public class Choice {
+    private int textId;
+    private int nextPage;
+}
--- InteractiveStory/app/src/main/java/com/example/yhussein/interactivestory/model/Page.java
+++ InteractiveStory/app/src/main/java/com/example/yhussein/interactivestory/model/Page.java
@@ -1,5 +1,43 @@
 package com.example.yhussein.interactivestory.model;
 
 public class Page {
+
     private int imageId;
+    private int textId;
+    private Choice choice1;
+    private Choice choice2;
+
+    // GETTER and SETTER methods
+
+    public int getImageId() {
+        return imageId;
+    }
+
+    public void setImageId(int imageId) {
+        this.imageId = imageId;
+    }
+
+    public int getTextId() {
+        return textId;
+    }
+
+    public void setTextId(int textId) {
+        this.textId = textId;
+    }
+
+    public Choice getChoice1() {
+        return choice1;
+    }
+
+    public void setChoice1(Choice choice1) {
+        this.choice1 = choice1;
+    }
+
+    public Choice getChoice2() {
+        return choice2;
+    }
+
+    public void setChoice2(Choice choice2) {
+        this.choice2 = choice2;
+    }
 }
