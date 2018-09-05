1523458d60b6c1d837fd1d13b17b0df88cea0bac
Yahya Hussein <yahyahussein91@gmail.com>
2018-09-04 23:53:39 -0700

    finished with intent

--- InteractiveStory/app/src/main/java/com/example/yhussein/interactivestory/StoryAcitivity.java
+++ InteractiveStory/app/src/main/java/com/example/yhussein/interactivestory/StoryAcitivity.java
@@ -1,13 +1,22 @@
 package com.example.yhussein.interactivestory;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 
 public class StoryAcitivity extends AppCompatActivity {
+    public static final String TAG = StoryAcitivity.class.getSimpleName();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_story_acitivity);
+
+        // GET INTENT
+        Intent intent = getIntent();
+        String name = intent.getStringExtra("name");
+        // Log Statement
+        Log.d(TAG, name);
     }
 }
