68a73ead327672b005cc1fd2eea1fe875f386f8d
Yahya Hussein <yahyahussein91@gmail.com>
2018-09-06 15:09:26 -0700

    done for the moment

--- Stormy/app/src/main/java/com/example/yhussein/stormy/AlertDialogFragment.java
+++ Stormy/app/src/main/java/com/example/yhussein/stormy/AlertDialogFragment.java
@@ -3,12 +3,14 @@ package com.example.yhussein.stormy;
 import android.app.AlertDialog;
 import android.app.Dialog;
 import android.app.DialogFragment;
+import android.content.Context;
 import android.os.Bundle;
 
 public class AlertDialogFragment extends DialogFragment {
     @Override
     public Dialog onCreateDialog(Bundle savedInstanceState) {
+        Context context = getActivity();
         //Alert Dialog
-        AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
+        AlertDialog.Builder builder = new AlertDialog.Builder(context);
     }
 }
