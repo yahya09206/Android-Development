8227a2910e38150a782ada2b5e8adcf0dd06b3e4
Yahya Hussein <yahyahussein91@gmail.com>
2018-09-06 14:33:25 -0700

    set up request to test

--- Stormy/app/src/main/java/com/example/yhussein/stormy/MainActivity.java
+++ Stormy/app/src/main/java/com/example/yhussein/stormy/MainActivity.java
@@ -2,12 +2,45 @@ package com.example.yhussein.stormy;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+
+import java.io.IOException;
+
+import okhttp3.Call;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
 
 public class MainActivity extends AppCompatActivity {
+    public static final String TAG = MainActivity.class.getSimpleName();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        String apiKey = "e59741b20df0b6a1feb5d659d327094b";
+
+        double latitude = 37.8267;
+        double longitude = -122.4233;
+
+        // Fake api call
+        String forecastURL = "https://api.darksky.net/forecast/"
+                + apiKey + "/" + latitude + "," + longitude;
+
+        // New HTTP Client object
+        OkHttpClient client = new OkHttpClient();
+        // Build Request
+        Request request = new Request.Builder()
+                .url(forecastURL)
+                .build();
+        // Put request inside of call object to get response
+        Call call = client.newCall(request);
+
+        try {
+            Response response = call.execute();
+        } catch (IOException e) {
+            Log.e(TAG, "IO Exception caught: ", e);
+        }
     }
 }
