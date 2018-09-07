457d0aed385f7926bf2bf851cb5db471f3658f56
Yahya Hussein <yahyahussein91@gmail.com>
2018-09-06 23:00:59 -0700

    fixed persmission error

--- Stormy/app/src/main/java/com/example/yhussein/stormy/MainActivity.java
+++ Stormy/app/src/main/java/com/example/yhussein/stormy/MainActivity.java
@@ -72,6 +72,11 @@ public class MainActivity extends AppCompatActivity {
     private boolean isNetworkAvailable() {
         ConnectivityManager manager = (ConnectivityManager) getSystemService(Context.CONNECTIVITY_SERVICE);
         NetworkInfo networkInfo = manager.getActiveNetworkInfo();
+        boolean isAvailable = false;
+        if (networkInfo != null && networkInfo.isConnected()){
+            isAvailable = true;
+        }
+        return isAvailable;
     }
 
     private void alertUserAboutError() {
