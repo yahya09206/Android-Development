09ada9234e5c37b6d62571d63ddafb426436cfbe
Yahya Hussein <yahyahussein91@gmail.com>
2018-09-07 23:17:24 -0700

    added member variables and their gets and sets

--- Stormy/app/src/main/java/com/example/yhussein/stormy/CurrentWeather.java
+++ Stormy/app/src/main/java/com/example/yhussein/stormy/CurrentWeather.java
@@ -1,4 +1,68 @@
 package com.example.yhussein.stormy;
 
 public class CurrentWeather {
+
+    private String locationLabel;
+    private String icon;
+    private long time;
+    private double temperature;
+    private double humidity;
+    private double percepChance;
+    private String summary;
+
+    public String getLocationLabel() {
+        return locationLabel;
+    }
+
+    public void setLocationLabel(String locationLabel) {
+        this.locationLabel = locationLabel;
+    }
+
+    public String getIcon() {
+        return icon;
+    }
+
+    public void setIcon(String icon) {
+        this.icon = icon;
+    }
+
+    public long getTime() {
+        return time;
+    }
+
+    public void setTime(long time) {
+        this.time = time;
+    }
+
+    public double getTemperature() {
+        return temperature;
+    }
+
+    public void setTemperature(double temperature) {
+        this.temperature = temperature;
+    }
+
+    public double getHumidity() {
+        return humidity;
+    }
+
+    public void setHumidity(double humidity) {
+        this.humidity = humidity;
+    }
+
+    public double getPercepChance() {
+        return percepChance;
+    }
+
+    public void setPercepChance(double percepChance) {
+        this.percepChance = percepChance;
+    }
+
+    public String getSummary() {
+        return summary;
+    }
+
+    public void setSummary(String summary) {
+        this.summary = summary;
+    }
 }
