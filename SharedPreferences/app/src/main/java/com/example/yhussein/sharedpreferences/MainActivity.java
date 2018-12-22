2ea94f46bd6535e5ceafd1fa625c17298e488100
Yahya Hussein <yahyahussein91@gmail.com>
2018-12-21 23:23:31 -0800

    changed to relative layout

--- SharedPreferences/app/src/main/java/com/example/yhussein/sharedpreferences/MainActivity.java
+++ SharedPreferences/app/src/main/java/com/example/yhussein/sharedpreferences/MainActivity.java
@@ -2,12 +2,16 @@ package com.example.yhussein.sharedpreferences;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
+    private EditText EditText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        EditText = findViewById(R.id.editText);
     }
 }
