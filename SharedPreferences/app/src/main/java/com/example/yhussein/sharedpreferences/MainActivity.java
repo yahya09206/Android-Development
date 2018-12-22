a622e4a1838f79f9347e14976ea783d06950bea2
Yahya Hussein <yahyahussein91@gmail.com>
2018-12-21 23:30:40 -0800

    added constant var for prefs

--- SharedPreferences/app/src/main/java/com/example/yhussein/sharedpreferences/MainActivity.java
+++ SharedPreferences/app/src/main/java/com/example/yhussein/sharedpreferences/MainActivity.java
@@ -1,10 +1,14 @@
 package com.example.yhussein.sharedpreferences;
 
+import android.content.Context;
+import android.content.SharedPreferences;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
+    private static final String PREFS_FILE = "com.example.yhussein.sharedpreferences.preferences";
+    private SharedPreferences SharedPreferences;
     private EditText EditText;
 
     @Override
@@ -12,6 +16,8 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        EditText = findViewById(R.id.editText);
+        EditText = findViewById(R.id.editText3);
+        SharedPreferences = getSharedPreferences(PREFS_FILE, Context.MODE_PRIVATE)
+;
     }
 }
